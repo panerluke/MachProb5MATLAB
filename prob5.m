b5145c057d56ad602c02f784029dfa79d31fefc8
panerluke <47804291+panerluke@users.noreply.github.com>
2019-12-07 18:12:14 +0800

    Add files via upload

--- prob5.m
+++ prob5.m
@@ -5,7 +5,7 @@ x = input('Please input function x(n): ');
 for z = 1:200
     if y(z) == 0
         y(z) = -1.5*x(z) + 2*x(z + 1) - 0.5*x(z + 2);
-    elseif y(z) > 0 && y(z) < 199
+    elseif y(z) > 0 && y(z) <= 198
         y(z) = 0.5*x(z + 1) - 0.5*x(z - 1);
     elseif y(z) == 199
         y(z) = 1.5*x(z) - 2*x(z - 1) + 0.5*x(z - 2);
